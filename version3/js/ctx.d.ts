06fc25e7986053c4d3df50fef30f7333cd6c0b9c
Marin Petrunić <marin.petrunic@gmail.com>
2019-05-04 16:15:48 +0200

    extend typescript definition

--- version3/js/ctx.d.ts
+++ version3/js/ctx.d.ts
@@ -6,13 +6,41 @@ export class ECP {
 
     static generator(): ECP;
 
+    static fromBytes(array: Uint8Array): ECP;
+
+    static mapit(hash: Uint8Array): ECP;
+
     is_infinity(): boolean;
+
+    copy(p: ECP): void;
+
+    neg(): void;
+
+    inf(): void;
+
+    affine(): void;
+
+    getX(): ECP;
+
+    getY(): ECP;
+
+    getS(): boolean;
+
+    add(p: ECP|ECP2): void;
+
+    sub(p: ECP|ECP2): void;
 
     mul(a: BIG): ECP;
 
     getX(): ECP;
 
     toBytes(): Uint8Array;
+
+    equals(p: ECP): boolean;
+
+}
+
+export class ECP2 {
 
 }
 
